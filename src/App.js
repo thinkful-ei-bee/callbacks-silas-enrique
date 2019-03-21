7b5845e861a478ad083e8080df58decba1335a18
Enrique Montemayor <eemontemayor@live.com>
2019-03-21 11:07:49 -0500

    second try

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import List from './List'
 import './App.css';
 
 class App extends Component {
+  state = {lists: [
+    {
+      id: '1',
+      header: 'First list',
+      cardIds: [ 'a', 'b', 'e', 'f', 'g', 'j', 'l', 'm' ],
+    },
+    {
+      id: '2',
+      header: 'Second list',
+      cardIds: ['b', 'c', 'd', 'f', 'h', 'i', 'k'],
+    },
+    {
+      id: '3',
+      header: 'Third list',
+      cardIds: [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm' ],
+    },
+    {
+      id: '4',
+      header: 'Fourth list',
+      cardIds: [ 'l', 'm' ],
+    },
+  ],
+  allCards: {
+    'a': { id: 'a', title: 'First card', content: 'lorem ipsum' },
+    'b': { id: 'b', title: 'Second card', content: 'lorem ipsum' },
+    'c': { id: 'c', title: 'Third card', content: 'lorem ipsum' },
+    'd': { id: 'd', title: 'Fourth card', content: 'lorem ipsum' },
+    'e': { id: 'e', title: 'Fifth card', content: 'lorem ipsum' },
+    'f': { id: 'f', title: 'Sixth card', content: 'lorem ipsum' },
+    'g': { id: 'g', title: 'Seventh card', content: 'lorem ipsum' },
+    'h': { id: 'h', title: 'Eighth card', content: 'lorem ipsum' },
+    'i': { id: 'i', title: 'Ninth card', content: 'lorem ipsum' },
+    'j': { id: 'j', title: 'Tenth card', content: 'lorem ipsum' },
+    'k': { id: 'k', title: 'Eleventh card', content: 'lorem ipsum' },
+    'l': { id: 'l', title: 'Twelfth card', content: 'lorem ipsum' },
+    'm': { id: 'm', title: 'Thirteenth card', content: 'lorem ipsum' },
+  },
+}
+
+newRandomCard = () => {
+  const id = Math.random().toString(36).substring(2, 4)
+    + Math.random().toString(36).substring(2, 4);
+  return {
+    id,
+    title: `Random Card ${id}`,
+    content: 'lorem ipsum',
+  }
+}
+
+
   render() {
+    
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <main className='App'>
+        <header className='App-header'>
+          <h1>Trelloyes!</h1>
         </header>
-      </div>
+        <div className='App-list'>
+          {this.state.lists.map(list => (
+            <List
+              onRandomClick = {this.newRandomCard}
+              key={list.id}
+              header={list.header}
+              cards={list.cardIds.map(id => this.state.allCards[id])}
+            />
+          ))}
+        </div>
+      </main>
     );
   }
 }
